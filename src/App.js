400d6d343d63bf0ed773513e4943be425b8903ff
Kearabetswejabane <keahjabane@gmail.com>
2024-06-18 18:25:47 +0200

    Added Git repo Url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/Kearabetswejabane" target="blank">
+          Kearabetswe Jabane
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/Kearabetswejabane" target="blank">
+          Open Sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
