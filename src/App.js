90075bb3d0455c3b16ca59aaf6a459019802e3b7
Kearabetswejabane <keahjabane@gmail.com>
2024-06-20 17:36:58 +0200

    Fixed Git Url

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <Weather />
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/Kearabetswejabane" target="blank">
-          Kearabetswe Jabane
-        </a>{" "}
-        and is{" "}
-        <a href="https://github.com/Kearabetswejabane" target="blank">
-          Open Sourced on Github
-        </a>
-      </footer>
+      <div className="Container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/Kearabetswejabane" target="blank">
+            Kearabetswe Jabane
+          </a>{" "}
+          and is{" "}
+          <a href="https://github.com/Kearabetswejabane" target="blank">
+            Open Sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
