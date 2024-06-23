70a1ca637669c52934a4e6fac7c28b01e3495617
Kearabetswejabane <keahjabane@gmail.com>
2024-06-23 15:23:58 +0200

    Added a Date

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <div className="Container">
+      <div className="Container Center">
         <h1>Weather App</h1>
         <Weather defaultCity="New York" />
         <footer>
